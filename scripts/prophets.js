019a44262640093a4b6c7b296fbac79d3b5888e7
danielclaudio08 <u262229862@byui.edu>
2026-03-18 00:04:22 +0800

    /

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,3 +1,15 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.table(data.prophets);
+  displayProphets(data.prophets);
+}
+
+getProphetData();
+
 const displayProphets = (prophets) => {
   prophets.forEach((prophet) => {
     // card build code goes here
@@ -25,17 +37,5 @@ const displayProphets = (prophets) => {
     cards.appendChild(card);
   });
 }
-
-const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
-const cards = document.querySelector('#cards');
-
-async function getProphetData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  // console.table(data.prophets);
-  displayProphets(data.prophets);
-}
-
-getProphetData();
 
 
